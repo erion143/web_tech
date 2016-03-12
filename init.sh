f69e43974c2b44b0a27a513b5ed0fa8bcf88cb78
erion143 <erion143@yandex.ru>
2016-03-12 22:55:59 +0300

    hz

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-cd ask
-sudo bash gunicorn_start.sh
+sudo /etc/init.d/gunicorn restart
