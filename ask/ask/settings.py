74956abdd017fd6672da8017ef72b8025908fdb7
erion143 <erion143@yandex.ru>
2016-03-16 22:01:16 +0300

    downgade django versiob to 1.6.1 Сил моих больше нет

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -2,10 +2,10 @@
 Django settings for ask project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
+https://docs.djangoproject.com/en/1.6/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
+https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -14,10 +14,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nf+*#v*4pf#f6z)5dbbga_!k9st(ndok(7i3kx1jpotbiku)-)'
+SECRET_KEY = '6_*=0-sfgf6%^ous)_fx$p0(4^@)511t3yj9x4curz&w!__9t&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,14 +30,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'qa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'autofixture',
+    'qa',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -45,7 +44,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -56,7 +54,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -68,7 +66,7 @@ DATABASES = {
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -82,6 +80,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import patterns, include, url
+
 from django.contrib import admin
+admin.autodiscover()
 
-
+urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-urlpatterns = patterns('',
-                       #url(r'^admin/', include(admin.site.urls)),
-                       url(r'^$',
-                           'qa.views.base_pagin'),
-                       url(r'^popular\/$',
-                           'qa.views.base_popular'),
-                       url(r'^question\/(?P<slug>\d+)\/$',
-                           'qa.views.post_details')
-                       )
-                       
+
+    url(r'^$',
+        'qa.views.base_pagin'),
+    url(r'^popular\/$',
+        'qa.views.base_popular'),
+    url(r'^question\/(?P<slug>\d+)\/$',
+        'qa.views.post_details')
+)
